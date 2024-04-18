f369d34d23f7caf9d8ef06be8753fc9d08e3445b
Sergei Dimitriev <wangraf@yandex.ru>
2024-04-19 00:23:45 +0300

    подаправил модуль поиска фразы в документе

--- parsing/parser_web.py
+++ parsing/parser_web.py
@@ -5,14 +5,20 @@ import pandas as pd
 import pdfplumber
 import textwrap
 import win32com.client as win32
+
+
+import nltk
+# nltk.download('punkt')
+
 from loguru import logger
+
+
 from config import ConfigSettings
 
 
 class DocumentSearcher:
     def __init__(self):
         self.directory = ConfigSettings.get_config_value('unpacked_output_local_directory')
-        self.checked_files = set()
 
     def format_text(self, text, width=110):
         """Форматирует текст, добавляя переносы строк каждые 'width' символов."""
@@ -23,21 +29,13 @@ class DocumentSearcher:
         for root, dirs, files in os.walk(self.directory):
             for file in files:
                 file_path = os.path.join(root, file)
-                if file_path in self.checked_files:
-                    logger.debug(f"Файл {file_path} уже проверен.")
-                    continue
                 file_extension = os.path.splitext(file)[1].lower()
-                if file_extension in ('.docx', '.doc', '.xlsx', '.xls', '.pdf'):
-                    found = False
-                    if file_extension in ('.docx', '.doc'):
-                        found = self.search_in_docx(file_path, phrases)
-                    elif file_extension in ('.xlsx', '.xls'):
-                        found = self.search_in_excel(file_path, phrases)
-                    elif file_extension == '.pdf':
-                        found = self.search_in_pdf(file_path, phrases)
-
-                    if found:
-                        self.checked_files.add(file_path)
+                if file_extension in ('.docx', '.doc'):
+                    self.search_in_docx(file_path, phrases)
+                elif file_extension in ('.xlsx', '.xls'):
+                    self.search_in_excel(file_path, phrases)
+                elif file_extension == '.pdf':
+                    self.search_in_pdf(file_path, phrases)
 
     def read_doc_file(self, file_path):
         """Читает текст из документа Word (.doc) с помощью COM автоматизации."""
@@ -56,7 +54,6 @@ class DocumentSearcher:
 
     def search_in_docx(self, file_path, phrases):
         """Поиск в документах Word."""
-        found = False
         file_extension = os.path.splitext(file_path)[1].lower()
         try:
             if file_extension == '.docx':
@@ -67,56 +64,53 @@ class DocumentSearcher:
                 text_blocks = [self.read_doc_file(file_path)]
             else:
                 logger.error(f"Неподдерживаемый формат файла {file_path}")
-                return found
+                return
 
             for text in text_blocks:
                 for phrase in phrases:
                     if phrase.lower() in text.lower():
-                        found = True
                         logger.info(self.format_text(f'Найдено в {file_path}: "{text}"'))
 
         except Exception as e:
             logger.error(f'Ошибка при поиске в файле {file_path}: {e}')
-        return found
 
     def search_in_excel(self, file_path, phrases):
         """Поиск в документах Excel."""
-        found = False
         try:
             xls = pd.ExcelFile(file_path)
             for sheet_name in xls.sheet_names:
                 df = pd.read_excel(xls, sheet_name)
+                # Очистка от NaN перед созданием строки
                 for index, row in df.iterrows():
+                    # Заменяем NaN на пустые строки и создаем единую строку
                     row_str = ' '.join(row.fillna('').astype(str))
+                    # Удаляем лишние пробелы, которые могли образоваться
                     row_str = re.sub(r'\s+', ' ', row_str).strip()
                     for phrase in phrases:
                         if phrase.lower() in row_str.lower():
-                            found = True
                             logger.info(self.format_text(f'Найдено в {file_path} на листе {sheet_name}: "{row_str}"'))
         except Exception as e:
             logger.error(f'Ошибка при поиске в файле {file_path}: {e}')
-        return found
 
     def search_in_pdf(self, file_path, phrase):
         """Поиск в PDF документах."""
-        found = False
         try:
             with pdfplumber.open(file_path) as pdf:
                 for page in pdf.pages:
                     text = page.extract_text()
                     if text:
+                        # Используем регулярное выражение для поиска контекста вокруг фразы
+                        # Слова до и после фразы (\w+\s+){0,10} - до и после до 10 слов
                         pattern = re.compile(r'(\w+\s+){0,10}' + re.escape(phrase) + r'(\s+\w+){0,10}', re.IGNORECASE)
                         results = pattern.findall(text)
                         for result in results:
-                            found = True
                             logger.info(
                                 f'Найдено в {file_path} на странице {page.page_number}: "{result[0].strip()} {phrase} {result[1].strip()}"')
         except Exception as e:
             logger.error(f'Ошибка при поиске в файле {file_path}: {e}')
-        return found
 
 
 if __name__ == "__main__":
     searcher = DocumentSearcher()
-    phrases = ['светильник', 'светильники', 'светильников', 'светодиод', 'освещение', 'светильниками', 'осветить', 'светодиодный']
+    phrases = ['светильник', 'светильники', 'светодиод', 'освещение', 'архимет', 'фонарь', 'led', 'лм']
     searcher.search_documents(phrases)
