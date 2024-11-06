378a498236f85efc3a0c58e19f05aa2f43eb8e59
Sergei Dimitriev <wangraf@yandex.ru>
2024-11-06 17:37:14 +0300

    для модуля downloading_files_from_links.py.py добавлена аннотация в классe DownloadFilesLinks для функции download_and_process_documents

--- download_files/downloading_files_from_links.py
+++ download_files/downloading_files_from_links.py
@@ -67,28 +67,44 @@ class DownloadFilesLinks():
     def download_and_process_documents(self):
         """
         Получает ссылки, скачивает документы и обрабатывает их.
+
+        Этот метод извлекает ссылки на документы из базы данных, проверяет доступность URL,
+        скачивает файлы, сохраняет их на диск, и обрабатывает ошибки, если они возникают.
+
+        Параметры:
+            Нет.
+
+        Возвращает:
+            Нет. Этот метод выполняет действия, такие как скачивание и обработка файлов.
         """
         try:
+            # Извлечение данных из базы данных
             results = self.db_manager.fetch_contract_data()
             logger.debug(f"Fetched {len(results)} records from the database.")
 
+            # Устанавливаем заголовки для HTTP-запросов
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
 
+            # Создаем директорию для хранения ZIP файлов, если она не существует
             if not os.path.exists(self.zip_directory):
                 os.makedirs(self.zip_directory)
                 logger.debug(f"Created directory {self.zip_directory}")
 
+            # Прогресс-бар для отслеживания обработки контрактов
             with tqdm(total=len(results), desc="Обработка контрактов", unit="контракт") as pbar:
+                # Обработка каждого контракта из базы данных
                 for data_id, documentation_links in results:
                     logger.debug(f"Processing data_id: {data_id}")
                     logger.debug(f"Documentation links: {documentation_links}")
 
+                    # Преобразование строкового представления ссылок в список
                     if isinstance(documentation_links, str):
                         documentation_links = [link.strip() for link in documentation_links.strip('[]').split(',')]
                         logger.debug(f"Parsed documentation_links: {documentation_links}")
 
+                    # Скачивание файлов по каждой ссылке
                     for link in documentation_links:
                         if 'file' in link:
                             try:
@@ -97,10 +113,11 @@ class DownloadFilesLinks():
                                 head_response = requests.head(link, headers=headers)
                                 head_response.raise_for_status()
 
+                                # Скачивание файла по ссылке
                                 response = requests.get(link, headers=headers, stream=True)
                                 response.raise_for_status()
 
-                                # Определение имени файла и его расширения
+                                # Определение имени файла из заголовков или URL
                                 content_disposition = response.headers.get('content-disposition')
                                 if content_disposition:
                                     filename = re.findall("filename\*?=['\"]?([^;'\"]+)", content_disposition)
@@ -112,10 +129,11 @@ class DownloadFilesLinks():
                                 else:
                                     filename = os.path.basename(link.split('?')[0])
 
+                                # Путь для сохранения файла
                                 file_path = os.path.join(self.zip_directory, filename)
                                 logger.debug(f"Saving file to: {file_path}")
 
-                                # Запись содержимого в файл
+                                # Запись содержимого файла на диск
                                 with open(file_path, 'wb') as file:
                                     for chunk in response.iter_content(chunk_size=8192):
                                         file.write(chunk)
@@ -123,12 +141,17 @@ class DownloadFilesLinks():
                                 logger.debug(f"Файл {filename} успешно скачан.")
 
                             except Exception as e:
+                                # Обработка ошибок при скачивании
                                 logger.error(f"Ошибка при скачивании файла по ссылке {link}: {e}")
 
+                    # Обновление прогресс-бара
                     pbar.update(1)
-                self.db_manager.close()
+
+            # Закрытие подключения к базе данных
+            self.db_manager.close()
 
         except Exception as e:
+            # Обработка ошибок при обработке документов
             logger.error(f"Ошибка при обработке документов: {e}")
 
 # # Пример использования
